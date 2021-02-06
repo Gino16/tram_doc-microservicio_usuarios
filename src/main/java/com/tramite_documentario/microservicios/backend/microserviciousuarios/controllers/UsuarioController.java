4d50fc024bbfe26e216dab0b8b51f34e6e4bbb4d
Gino Ascencio <gino.francisco@hotmail.com>
2021-02-05 19:38:34 -0500

    Recuperacion y envio de contra con link

--- src/main/java/com/tramite_documentario/microservicios/backend/microserviciousuarios/controllers/UsuarioController.java
+++ src/main/java/com/tramite_documentario/microservicios/backend/microserviciousuarios/controllers/UsuarioController.java
@@ -8,11 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
